9862df77e2835486832fecce04b4335affa6eae9
Jendorski Labs <jendorskilabs@gmail.com>
2023-09-01 16:01:27 +0300

    - Updated usage of connection filter - Updated usage of invite

---
+++ src/services/connectionFilter.ts
@@ -0,0 +1,53 @@
+import { IConnectionFilter, Status } from "pakt-sdk";
+import { internalResponse } from "../utils";
+
+export const updateConnectionFilter = async () => {
+  try {
+    const payload: IConnectionFilter = {
+      event: "CREATE_CONVERSATION",
+      key: "afroScore",
+      value: 60,
+      decider: "greater_than",
+    };
+
+    const upd = await init.connectionFilter.update(payload);
+    if (upd.status === Status.ERROR)
+      return internalResponse(
+        true,
+        Number(upd.code ?? upd.statusCode),
+        String(upd.message),
+        upd
+      );
+    return internalResponse(
+      false,
+      Number(upd.code ?? upd.statusCode),
+      String(upd.message),
+      upd
+    );
+  } catch (error: Error | unknown) {
+    console.log("updateConnectionFilter: ", { error });
+    return internalResponse(true, 422, String(error), null);
+  }
+};
+
+export const getConnectionFilter = async () => {
+  try {
+    const get = await init.connectionFilter.getForAUser();
+    if (get.status === Status.ERROR)
+      return internalResponse(
+        true,
+        Number(get.code ?? get.statusCode),
+        String(get.message),
+        get
+      );
+    return internalResponse(
+      false,
+      Number(get.code ?? get.statusCode),
+      String(get.message),
+      get
+    );
+  } catch (error: Error | unknown) {
+    console.log("getConnectionFilter: ", { error });
+    return internalResponse(true, 422, String(error), null);
+  }
+};
---
+++ src/services/invite.ts
@@ -0,0 +1,114 @@
+import { IInviteDto, SendInviteDto, Status } from "pakt-sdk";
+import { internalResponse } from "../utils";
+
+export const sendInvite = async ({ payload }: { payload: SendInviteDto }) => {
+  try {
+    const send = await init.invite.sendInvite(payload);
+    if (send.status === Status.ERROR)
+      return internalResponse(
+        true,
+        Number(send.code ?? send.statusCode),
+        String(send.message),
+        send
+      );
+    return internalResponse(
+      false,
+      Number(send.code ?? send.statusCode),
+      String(send.message),
+      send
+    );
+  } catch (error: Error | unknown) {
+    console.log("sendInvite: ", { error });
+    return internalResponse(true, 422, String(error), null);
+  }
+};
+
+export const acceptInvite = async (inviteId: string) => {
+  try {
+    const accept = await init.invite.acceptInvite(inviteId);
+    if (accept.status === Status.ERROR)
+      return internalResponse(
+        true,
+        Number(accept.code ?? accept.statusCode),
+        String(accept.message),
+        accept
+      );
+    return internalResponse(
+      false,
+      Number(accept.code ?? accept.statusCode),
+      String(accept.message),
+      accept
+    );
+  } catch (error: Error | unknown) {
+    console.log("acceptInvite:, ", { error });
+    return internalResponse(true, 422, String(error), null);
+  }
+};
+
+export const declineInvite = async (inviteId: string) => {
+  try {
+    const accept = await init.invite.acceptInvite(inviteId);
+    if (accept.status === Status.ERROR)
+      return internalResponse(
+        true,
+        Number(accept.code ?? accept.statusCode),
+        String(accept.message),
+        accept
+      );
+    return internalResponse(
+      false,
+      Number(accept.code ?? accept.statusCode),
+      String(accept.message),
+      accept
+    );
+  } catch (error: Error | unknown) {
+    console.log("declineInvite:, ", { error });
+    return internalResponse(true, 422, String(error), null);
+  }
+};
+
+export const getAllInvites = async (
+  filter: Record<string, any> | IInviteDto
+) => {
+  try {
+    const gets = await init.invite.getAll(filter);
+    if (gets.status === Status.ERROR)
+      return internalResponse(
+        true,
+        Number(gets.code ?? gets.statusCode),
+        String(gets.message),
+        gets
+      );
+    return internalResponse(
+      false,
+      Number(gets.code ?? gets.statusCode),
+      String(gets.message),
+      gets
+    );
+  } catch (error: Error | unknown) {
+    console.log("getAllInvites:, ", { error });
+    return internalResponse(true, 422, String(error), null);
+  }
+};
+
+export const getAnInvite = async (inviteId: string) => {
+  try {
+    const anInvite = await init.invite.getAnInvite(inviteId);
+    if (anInvite.status === Status.ERROR)
+      return internalResponse(
+        true,
+        Number(anInvite.code ?? anInvite.statusCode),
+        String(anInvite.message),
+        anInvite
+      );
+    return internalResponse(
+      false,
+      Number(anInvite.code ?? anInvite.statusCode),
+      String(anInvite.message),
+      anInvite
+    );
+  } catch (error: Error | unknown) {
+    console.log("getAnInvite:, ", { error });
+    return internalResponse(true, 422, String(error), null);
+  }
+};
