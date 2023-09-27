604aa50c0e5d06f1df24993098245d495da2bc41
Jendorski Labs <jendorskilabs@gmail.com>
2023-09-28 00:09:04 +0300

    - Integrated feature to fetch file uploads - Integrated feature to fetch a file uploaded information

--- src/services/upload.ts
+++ src/services/upload.ts
@@ -1,4 +1,11 @@
-import { CreateFileUpload, IUploadDto, ResponseDto, Status } from "pakt-sdk";
+import {
+  CreateFileUpload,
+  FilterUploadDto,
+  FindUploadDto,
+  IUploadDto,
+  ResponseDto,
+  Status,
+} from "pakt-sdk";
 import { internalResponse } from "../utils";
 
 const TAG = "services/upload";
@@ -24,6 +31,56 @@ export const fileUpload = async ({
       Number(upload.code ?? upload.statusCode),
       String(upload.message),
       upload
+    );
+  } catch (error: Error | unknown) {
+    console.error(`${TAG}::${String(error)}`);
+    return internalResponse(true, 422, String(error), null);
+  }
+};
+
+export const fetchFileUploads = async (filter: FilterUploadDto) => {
+  try {
+    const sampleFilter = {
+      page: "1",
+      limit: "20",
+    };
+    const uploads: ResponseDto<FindUploadDto> = await init.file.getFileUploads(
+      filter
+    );
+    if (uploads.status === Status.ERROR)
+      return internalResponse(
+        true,
+        Number(uploads.code ?? uploads.statusCode),
+        String(uploads.message),
+        uploads
+      );
+    return internalResponse(
+      false,
+      Number(uploads.code ?? uploads.statusCode),
+      String(uploads.message),
+      uploads
+    );
+  } catch (error: Error | unknown) {
+    console.error(`${TAG}::${String(error)}`);
+    return internalResponse(true, 422, String(error), null);
+  }
+};
+
+export const fetchAFileUpload = async (id: string) => {
+  try {
+    const anUpload = await init.file.getAFileUpload(id);
+    if (anUpload.status === Status.ERROR)
+      return internalResponse(
+        true,
+        Number(anUpload.code ?? anUpload.statusCode),
+        String(anUpload.message),
+        anUpload
+      );
+    return internalResponse(
+      false,
+      Number(anUpload.code ?? anUpload.statusCode),
+      String(anUpload.message),
+      anUpload
     );
   } catch (error: Error | unknown) {
     console.error(`${TAG}::${String(error)}`);
